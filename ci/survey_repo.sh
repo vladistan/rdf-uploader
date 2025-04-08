b20cd34db30363e53eaf6a04d7d9511de2376677
Vlad Korolev <vlad@v-lad.org>
2025-04-07 23:46:32 -0400

    Add survey repo

---
+++ ci/survey_repo.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Borrowed from : https://github.com/Autorevision/autorevision/blob/master/autorevision.sh
+function gitRepo {
+
+    set +e +o pipefail
+    local oldPath="$(pwd)"
+
+    cd "$(git rev-parse --show-toplevel)"
+
+    local currentRev="$(git rev-parse HEAD)"
+
+    # Is the working copy clean?
+    test -z "$(git status --untracked-files=normal --porcelain)"
+    VCS_WC_MODIFIED="${?}"
+
+    # Enumeration of changesets
+    VCS_NUM="$(git rev-list --count "${currentRev}" 2>/dev/null)"
+    if [ -z "${VCS_NUM}" ]; then
+        VCS_NUM="$(git rev-list HEAD | wc -l)"
+    fi
+
+    # The short hash
+    VCS_SHORT_HASH="$(git rev-parse --short "${currentRev}")"
+
+    # Cache the description
+    local DESCRIPTION="$(git describe --long --tags "${currentRev}" 2>/dev/null)"
+    # Current or last tag ancestor (empty if no tags)
+    VCS_TAG="$(echo "${DESCRIPTION}" | sed -e "s:-g${VCS_SHORT_HASH}\$::" -e 's:-[0-9]*$::')"
+    # Distance to last tag or an alias of VCS_NUM if there is no tag
+    if [ -n "${DESCRIPTION}" ]; then
+        VCS_TICK="$(echo "${DESCRIPTION}" | sed -e "s:${VCS_TAG}-::" -e "s:-g${VCS_SHORT_HASH}::")"
+    else
+        VCS_TICK="${VCS_NUM}"
+    fi
+
+    cd "${oldPath}"
+
+    set -e -o pipefail
+
+}
+
+echo "Survey git repo"
+gitRepo
+
+
+VERSION="${VCS_TAG#v}.${VCS_TICK}"
+if [ "$VCS_WC_MODIFIED" == 1 ]; then
+    VERSION="${VERSION}-dirty"
+fi
+
+cat <<EOF
+
+  /\_/\     Version   : ${VERSION}
+ ( o.o )    Commit    : ${VCS_SHORT_HASH}
+  > ^ <     
+            
+
+EOF
